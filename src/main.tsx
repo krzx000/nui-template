c5cd72f8da99efdac825b905776d590819fb51e1
krzx <padlo.krzysiek@gmail.com>
2025-04-12 23:21:42 +0200

    Add useNuiEvent hook for managing event listeners from client scripts

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
+import { isEnvBrowser } from "./utils/misc.ts";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+let root = document.getElementById("root");
+
+if (isEnvBrowser()) {
+  root!.style.backgroundImage = 'url("https://i.imgur.com/3pzRj9n.png")'; // day
+  // root!.style.backgroundImage = 'url("https://i.imgur.com/iPTAdYV.png")'; // night
+  root!.style.backgroundSize = "cover";
+  root!.style.backgroundRepeat = "no-repeat";
+  root!.style.backgroundPosition = "center";
+}
+
+ReactDOM.createRoot(root!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
